3e0ccb36c04c18fc95089fcdc6ee51fb76fc102f
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-04-16 23:55:03 +0900

    Feat: Add dependency for shared_preferences
    Install shared_preferences and load in dependencies
    Add method addToCartList from cart_repo

--- lib/data/repository/cart_repo.dart
+++ lib/data/repository/cart_repo.dart
@@ -1,1 +1,28 @@
-class CartRepo {}
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/models/models.dart';
+import 'package:food_delivery_app/utils/utils.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CartRepo {
+  final SharedPreferences sharedPreferences;
+
+  CartRepo({
+    required this.sharedPreferences,
+  });
+
+  List<String> cart = [];
+
+  void addToCartList(List<CartModel> cartList) {
+    cart = [];
+    /*
+      convert objects to string because sharedPreference only accepts string
+     */
+    for (var element in cartList) {
+      cart.add(jsonEncode(element));
+    }
+
+    sharedPreferences.setStringList(AppConstants.CART_LIST, cart);
+    debugPrint('${sharedPreferences.getStringList(AppConstants.CART_LIST)}');
+  }
+}
--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -3,15 +3,19 @@ import 'package:food_delivery_app/data/api/api_client.dart';
 import 'package:food_delivery_app/data/repository/repository.dart';
 import 'package:food_delivery_app/utils/utils.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> init() async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+  Get.lazyPut(() => sharedPreferences);
+
   // api client
   Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
   // repos
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
   Get.lazyPut(() => RecommendedProductRepo(apiClient: Get.find()));
-  Get.lazyPut(() => CartRepo());
+  Get.lazyPut(() => CartRepo(sharedPreferences: Get.find()));
 
   // controllers
   Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
